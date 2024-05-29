ccdcd70c9c5802c1e11dcc6bc169fc75eb8884c9
Lapis <88260244+LapisPhoenix@users.noreply.github.com>
2024-05-28 19:22:12 -0700

    Added Quote Command
    Added Quote command & quote txt file

---
+++ cogs/fun/quote.py
@@ -0,0 +1,26 @@
+from typing import TYPE_CHECKING
+from random import choice
+from discord.ext import commands
+from discord import app_commands, Interaction
+
+
+if TYPE_CHECKING:
+    from main import Sassy
+
+
+class Quote(commands.Cog):
+    def __init__(self, bot: 'Sassy'):
+        self.bot = bot
+
+    @app_commands.command(name="quote", description="You want some advice from me?")
+    async def quote(self, inter: Interaction):
+        with open("cogs/fun/quote.txt", "r") as f:
+            quotes = f.readlines()
+
+        selected_quote = choice(quotes)
+
+        await inter.response.send_message(selected_quote)
+
+
+async def setup(bot: 'Sassy'):
+    await bot.add_cog(Quote(bot))
